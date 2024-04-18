a73bd7ebfadb09a76bb9260b32819075369c0ad4
Mojtaba Movahedian <44787178+MMojtaba@users.noreply.github.com>
2024-04-17 21:33:25 -0700

    added angular ui router

--- client/client.js
+++ client/client.js
@@ -1,6 +1,13 @@
-angular.module("PaintingsApp", []).controller("MainCtrl", [
-  "$scope",
-  function ($scope) {
-    $scope.message = "wow inside frontend";
-  }
-]);
+const app = angular.module("PaintingsApp", ["ui.router"]);
+
+app.config(function ($stateProvider) {
+  $stateProvider.state("Home", {
+    url: "/home",
+    template: "<h1>Welcome to home!</h1>"
+  });
+
+  $stateProvider.state("Browse", {
+    url: "/browse",
+    template: "<h1>Welcome to browse!</h1>"
+  });
+});
