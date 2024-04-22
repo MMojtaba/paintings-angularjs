5fee77f5ecdb1e144a0ad103999aaaedde3b996c
Mojtaba Movahedian <44787178+MMojtaba@users.noreply.github.com>
2024-04-21 18:36:04 -0700

    small changes

--- client/controllers/login.js
+++ client/controllers/login.js
@@ -1,6 +1,13 @@
 angular.module("PaintingsApp").controller("LoginCtrl", [
   "$scope",
   function ($scope) {
-    //
+    $scope.state = {
+      username: "",
+      password: ""
+    };
+
+    $scope.login = async function () {
+      console.log("logging in");
+    };
   }
 ]);
