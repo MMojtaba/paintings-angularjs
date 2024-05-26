e8b1a6187e0a0dcb5ba440c8f0f6fe1d41a3a546
Mojtaba Movahedian <44787178+MMojtaba@users.noreply.github.com>
2024-05-26 14:06:37 -0700

    animated the changing of the selected image in home and made it so it periodically changes the image

--- client/controllers/home.js
+++ client/controllers/home.js
@@ -12,6 +12,8 @@ angular.module("PaintingsApp").controller("HomeCtrl", [
       isLoading: true,
     };
 
+    let currImageIndex = 1;
+
     async function init() {
       const query = {
         limit: 4,
@@ -41,15 +43,46 @@ angular.module("PaintingsApp").controller("HomeCtrl", [
           if (err.status !== 404)
             console.error("Error getting featured images.", err);
         });
+
+      // Run the change image function periodically
+      const changeImageIntervalid = setInterval(changeImage, 5000);
     }
     init();
 
+    // Changes the image and animates it
+    async function animateAndChangeImage(image) {
+      const mainImageElement = document.getElementById("home-main-image");
+      if (mainImageElement) {
+        mainImageElement.style.filter = "brightness(0.7)";
+        await $timeout(() => {}, 250);
+        $scope.state.selectedImage = image;
+        await $timeout(() => {}, 250);
+        mainImageElement.style.filter = "brightness(1)";
+      }
+    }
+
+    // Changes the main image
+    function changeImage() {
+      // use the featured images if they exist
+      if ($scope.state.featured?.length) {
+        const length = $scope.state.featured.length;
+        animateAndChangeImage($scope.state.featured[currImageIndex % length]);
+        currImageIndex++;
+      } else if ($scope.state.images?.length) {
+        const length = $scope.state.images.length;
+        animateAndChangeImage($scope.state.images[currImageIndex % length]);
+        currImageIndex++;
+      }
+    }
+
     $scope.handleImageClick = function (image) {
       // Go to image preview if clicking the selected image
-      if ($scope.state.selectedImage === image)
+      if ($scope.state.selectedImage === image) {
         $state.go("ImagePreview", { id: image.fileId });
-      // Otherwise, set this as the selected image
-      else $scope.state.selectedImage = image;
+      } else {
+        // Otherwise, set this as the selected image
+        animateAndChangeImage(image);
+      }
     };
   },
 ]);
